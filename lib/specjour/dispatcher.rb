f0b750fc314b3f2ab9fa46b75b41ceddc6d0e744
Jack Chen (chendo) <me@chen.do>
2011-06-08 15:27:17 +1000

    DNSSD's more_coming? flag seems to be broken on Linux hosts

--- lib/specjour/dispatcher.rb
+++ lib/specjour/dispatcher.rb
@@ -111,12 +111,12 @@ module Specjour
 
     def gather_remote_managers
       browser = DNSSD::Service.new
-      Timeout.timeout(3) do
+      Timeout.timeout(5) do
         browser.browse '_druby._tcp' do |reply|
           if reply.flags.add?
             resolve_reply(reply)
           end
-          browser.stop unless reply.flags.more_coming?
+          browser.stop unless reply.flags.more_coming? or RUBY_PLATFORM =~ /linux/
         end
       end
       rescue Timeout::Error
